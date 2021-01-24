c6ddd059ebd29eedf8a53812a643511fbd8b4a15
juliaumar <juliaumar71@gmail.com juliaumar71@gmail.com git config --global user.email juliaumar71@gmail.comgit config --global user.email juliaumar71@gmail.com git config --global user.name Julia>
2021-01-24 18:02:47 -0500

    Loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,12 +1,16 @@
 import com.sun.org.apache.xpath.internal.operations.String;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ForLoop {
     public static void main(String[] args) {
-        int [] lots = {2, 5, 3, 7, 9, 1};
+        int [] lots = {1, 2, 3, 4, 5};
+
 
         for (int i = 0; i < lots.length ; i++) {
 
-            System.out.println(lots.length);
+            System.out.println(Arrays.toString(lots));
 
         }
     }
