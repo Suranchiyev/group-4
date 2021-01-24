dbd209d96d572dbcedc74456f8b165073598f6c1
juliaumar <juliaumar71@gmail.com juliaumar71@gmail.com git config --global user.email juliaumar71@gmail.comgit config --global user.email juliaumar71@gmail.com git config --global user.name Julia>
2021-01-24 17:50:57 -0500

    Loop

---
+++ src/ForLoop.java
@@ -0,0 +1,13 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int [] lots = {2, 5, 3, 7, 9, 1};
+
+        for (int i = 0; i < lots.length ; i++) {
+
+            System.out.println(lots.length);
+
+        }
+    }
+}
